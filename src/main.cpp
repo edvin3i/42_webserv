60bc34c5ac8905aa6f4787b557f689ff12f33819
gbreana <gbreana@student.42.fr>
2024-11-16 20:55:50 +0100

    test some server params in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,10 @@
 
 
 int main(int argc, char **argv) {
-//	using namespace http;
-//
-//	TcpServer server = TcpServer("127.0.0.1", 8080);
-//	server.startListen();
+		std::cout << "ERROR: wrong number of arguments!" << std::endl;
+
 
 	if (argc != 2) {
-		std::cout << "ERROR: wrong number of arguments!" << std::endl;
 		std::cout << "USAGE: ./webserv <config.cfg>" << std::endl;
 		return 1;
 	}
@@ -25,8 +22,12 @@ int main(int argc, char **argv) {
 	std::vector<ServerConfig> servers_configs = conf_parser.getConfig();
 	servers_configs[0].print_server_config();
 	servers_configs[1].print_server_config();
+	std::cout << servers_configs[1].locations[0].autoindex << std::endl;
 
+	using namespace http;
 
+	TcpServer server = TcpServer(servers_configs[1].host, servers_configs[1].port);
+	server.startListen();
 
 
 

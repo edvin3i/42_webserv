62f18000d9a4da7187fa6cd2505b37afdd78170f
Antonin <antonin.guyon861@gmail.com>
2025-01-23 12:11:16 +0100

    add file website

---
+++ www/cgi/error.php
@@ -0,0 +1,9 @@
+<?php
+// Fichier avec une erreur fatale pour tester le serveur
+
+// Appel d'une fonction inexistante
+undefined_function_call();
+
+// Ce code ne sera pas exécuté
+echo "Ceci ne sera pas affiché.";
+?>
